622edf06b87f0c2e80cd7b7687f1cf2e801b0cd2
Awans <kuatriawan69@gmail.com>
2026-06-01 23:10:57 +0700

    Setup GitHub Pages deployment with gh-pages package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/Nuraga-Project/',
     plugins: [react()],
     resolve: {
         mainFields: ['module', 'main', 'browser'],
